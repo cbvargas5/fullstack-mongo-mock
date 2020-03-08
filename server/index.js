0ab0996e6d887342f6fcaa03f29ad18a20c329fa
Darth Varg <cbvargas123@gmail.com>
2020-03-08 05:04:09 -0700

    Server side index.js has been fixed; routes have all been put in route folder; packages save; working on database now

--- server/index.js
+++ server/index.js
@@ -2,10 +2,18 @@
 // FIX ME :(
   const express = require('express');
   const path = require('path');
-  const router = require('./router');
-
+  const bodyParser = require('body-parser');
   const server = express();
+  const port = 3000
+
+  server.use(bodyParser.json())
+  server.use(bodyParser.urlencoded({ extended: false }))
+
+  const router = require('./router');
+  server.use('/products', router)
   
+
+  //normally i would put these in their own route file but I'm not trying to move anything around
   server.use('/', express.static(path.join(__dirname + '/../client/dist')));
   
   server.get('/name', (req, res) => {
@@ -16,11 +24,11 @@
     res.status(200).send('This is your post request, modify this file to use your router!')
   })
   
-  server.put('/name', (req, res) => {
+  server.put('/name/:id', (req, res) => {
     res.status(200).send('This is your put request, modify this file to use your router!')
   })
   
-  server.delete('/name', (req, res) => {
+  server.delete('/name/:id', (req, res) => {
     res.status(200).send('This is your delete request, modify this file to use your router!')
   })
 
--- server/router.js
+++ server/router.js
@@ -6,11 +6,37 @@ const controller = require('./controller.js');
 router
   .route('/products')
   .get(controller.get)
-  .post(controller.post)
+  .post(controller.post);
 
 router
-  .route('/products/:_id')
+  .route('/products/:id')
   .put(controller.put)
-  .delete(controller.delete)
+  .delete(controller.delete);
+
+  router
+    .route('/name')
+    .get(() => res.status(200).send('This is your get request, modify this file to use your router!'))
+    .post(() => res.status(200).send('This is your post request, modify this file to use your router!'));
+
+  router
+    .route('/name/:id')
+    .put(() => res.status(200).send('This is your put request, modify this file to use your router!'))
+    .delete(() => res.status(200).send('This is your delete request, modify this file to use your router!'));
+
+  // server.get('/name', (req, res) => {
+  //   res.status(200).send('This is your get request, modify this file to use your router!')
+  // })
+  
+  // server.post('/name', (req, res) => {
+  //   res.status(200).send('This is your post request, modify this file to use your router!')
+  // })
+  
+  // server.put('/name/:id', (req, res) => {
+  //   res.status(200).send('This is your put request, modify this file to use your router!')
+  // })
+  
+  // server.delete('/name/:id', (req, res) => {
+  //   res.status(200).send('This is your delete request, modify this file to use your router!')
+  // })
 
 module.exports = router
